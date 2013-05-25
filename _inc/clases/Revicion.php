cc2aa5f540aea0b73bc27807d8810b6ed886d994
Sesai Quispe <sesaiquispe@gmail.com>
2013-05-25 11:13:43 -0400

    no message

---
+++ _inc/clases/Revicion.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Esta clase es para guardar los datos tipo Tutor
+ *
+ * @author Guyen Campero <guyencu@gmail.com>
+ */
+class Tutor extends Objectbase 
+{
+ /**
+  * Codigo identificador del Objeto Usuario
+  * @var INT(11)
+  */
+  var $revicion_id;
+}
+
+?>
